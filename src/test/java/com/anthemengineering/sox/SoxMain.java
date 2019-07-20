5a7b32dece22a950939c2481cbc596d5c2ec3f30
Alex Chin <alex.ba.chin@gmail.com>
2019-07-19 21:29:59 -0400

    Add error checking.

--- src/test/java/com/anthemengineering/sox/SoxMain.java
+++ src/test/java/com/anthemengineering/sox/SoxMain.java
@@ -4,16 +4,19 @@ import com.anthemengineering.sox.jna.*;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.PointerByReference;
 
+import static com.anthemengineering.sox.jna.SoxLibrary.sox_error_t.SOX_SUCCESS;
+
 public class SoxMain {
 
     public static final String SOURCE_PATH = "src/test/resources/ascending-fifths.wav";
     public static final String DESTINATION_PATH = "output.wav";
 
     public static void main(String[] args) {
-        SoxLibrary.INSTANCE.sox_init();
+        assert SoxLibrary.INSTANCE.sox_init() == SOX_SUCCESS;
         System.out.println("Initialized sox");
 
         sox_format_t source = SoxLibrary.INSTANCE.sox_open_read(SOURCE_PATH, null, null, null);
+        assert source != null;
         System.out.println("Opened " + SOURCE_PATH + ": " + source);
 
         sox_format_t destination = SoxLibrary.INSTANCE.sox_open_write(DESTINATION_PATH, source.signal, null, null, null, new SoxLibrary.sox_open_write_overwrite_permitted_callback() {
@@ -23,6 +26,7 @@ public class SoxMain {
                 return 1;
             }
         });
+        assert destination != null;
         System.out.println("Opened " + DESTINATION_PATH + ": " + destination);
 
         sox_effects_chain_t chain = SoxLibrary.INSTANCE.sox_create_effects_chain(source.encoding, destination.encoding);
@@ -30,7 +34,7 @@ public class SoxMain {
 
         sox_effect_handler_t inputEffectHandler = SoxLibrary.INSTANCE.sox_find_effect("input");
         sox_effect_t inputEffect = SoxLibrary.INSTANCE.sox_create_effect(inputEffectHandler);
-        SoxLibrary.INSTANCE.sox_effect_options(inputEffect, 1, new PointerByReference[] { new PointerByReference(source.getPointer()) });
+        assert SoxLibrary.INSTANCE.sox_effect_options(inputEffect, 1, new PointerByReference[] { new PointerByReference(source.getPointer()) }) == SOX_SUCCESS;
         System.out.println("Created input effect: " + inputEffect);
 
         SoxLibrary.INSTANCE.sox_add_effect(chain, inputEffect, source.signal, destination.signal);
@@ -38,7 +42,10 @@ public class SoxMain {
 
         sox_effect_handler_t outputEffectHandler = SoxLibrary.INSTANCE.sox_find_effect("output");
         sox_effect_t outputEffect = SoxLibrary.INSTANCE.sox_create_effect(outputEffectHandler);
-        SoxLibrary.INSTANCE.sox_effect_options(outputEffect, 0, (PointerByReference[]) null);
+        assert SoxLibrary.INSTANCE.sox_effect_options(outputEffect, 0, (PointerByReference[]) null) == SOX_SUCCESS;
         System.out.println("Created output effect: " + outputEffect);
+
+        SoxLibrary.INSTANCE.sox_flow_effects(chain, null, null);
+        System.out.println("Flowed chain");
     }
 }
